f85a3343b1c3235916b1553c1b2ee185c7e0ef5b
calllivecn <c-all@qq.com>
2017-05-04 19:46:43 +0800

    要提交的变更： 修改：     kernel_modules/hello.c
    未跟踪的文件:
    kernel_modules/makefile

--- kernel_modules/hello.c
+++ kernel_modules/hello.c
@@ -20,8 +20,8 @@ printk(KERN_ALERT " Hello World exit\n ");
 module_init(hello_init);
 module_exit(hello_exit);
 
-MODULE_AUTHOR("Song Baohua");
-MODULE_DESCRIPTION("A simple Hello World Module");
-MODULE_ALIAS("a simplest module");
+MODULE_AUTHOR("Song Baohua")
+MODULE_DESCRIPTION("A simple Hello World Module")
+MODULE_ALIAS("a simplest module")
 
 
